265c832f0b1b92e87e78e7785ff172251e64d9d0
Igor <ermygut@gmail.com>
2021-04-09 13:24:33 +0300

    sign in form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from './components/sidebar/Sidebar'
 import {BrowserRouter, Route} from 'react-router-dom'
 import ChartPage from './components/chart/chart'
 import MainPage from './components/mainPage/mainPage'
+import Signin from './components/user/userMain'
 
 export default class App extends React.Component {
  
@@ -11,6 +12,7 @@ export default class App extends React.Component {
   return (
     <BrowserRouter>
     <div>
+      <Signin/>
       <Sidebar/>
       <Route path='/' component={MainPage} exact/>    
       <Route path='/chart' component={ChartPage} exact/>    
