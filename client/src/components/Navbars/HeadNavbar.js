a7509c604066b962aa731d23c5b72aa8cf5e8b28
Igor <ermygut@gmail.com>
2021-08-29 15:49:12 +0300

    authentication

--- client/src/components/Navbars/HeadNavbar.js
+++ client/src/components/Navbars/HeadNavbar.js
@@ -1,162 +1,138 @@
-import React, {useContext} from "react"
-import classNames from "classnames"
-import {SignedIn} from "./IsUserDropdown"
-import NotLoggedIn from "./IsUserDropdown"
-import {AuthContext} from "../../contexts/auth.context"
+import React, { useContext } from 'react';
+import classNames from 'classnames';
+import { SignedIn } from './IsUserDropdown';
+
+import { AuthContext } from '../../contexts/auth.context';
 // reactstrap components
 import {
-  Button,
-  Collapse,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  UncontrolledDropdown,
-  Input,
-  InputGroup,
-  NavbarBrand,
-  Navbar,
-  NavLink,
-  Nav,
-  Container,
-  Modal,
-  NavbarToggler,
-  ModalHeader,
-} from "reactstrap";
+	Button,
+	Collapse,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem,
+	UncontrolledDropdown,
+	Input,
+	InputGroup,
+	NavbarBrand,
+	Navbar,
+	NavLink,
+	Nav,
+	Container,
+	Modal,
+	NavbarToggler,
+	ModalHeader
+} from 'reactstrap';
 
 function HeadNavbar(props) {
-  const {isAuthenticated} = useContext(AuthContext)
-  const [collapseOpen, setcollapseOpen] = React.useState(false);
-  const [modalSearch, setmodalSearch] = React.useState(false);
-  const [color, setcolor] = React.useState("navbar-transparent");
-  React.useEffect(() => {
-    window.addEventListener("resize", updateColor);
-    // Specify how to clean up after this effect:
-    return function cleanup() {
-      window.removeEventListener("resize", updateColor);
-    };
-  });
-  // function that adds color white/transparent to the navbar on resize (this is for the collapse)
-  const updateColor = () => {
-    if (window.innerWidth < 993 && collapseOpen) {
-      setcolor("bg-white");
-    } else {
-      setcolor("navbar-transparent");
-    }
-  };
-  // this function opens and closes the collapse on small devices
-  const toggleCollapse = () => {
-    if (collapseOpen) {
-      setcolor("navbar-transparent");
-    } else {
-      setcolor("bg-white");
-    }
-    setcollapseOpen(!collapseOpen);
-  };
-  // this function is to open the Search modal
-  const toggleModalSearch = () => {
-    setmodalSearch(!modalSearch);
-  };
+	const { isAuthenticated } = useContext(AuthContext);
+	const [collapseOpen, setcollapseOpen] = React.useState(false);
+	const [modalSearch, setmodalSearch] = React.useState(false);
+	const [color, setcolor] = React.useState('navbar-transparent');
+	React.useEffect(() => {
+		window.addEventListener('resize', updateColor);
+		// Specify how to clean up after this effect:
+		return function cleanup() {
+			window.removeEventListener('resize', updateColor);
+		};
+	});
+	// function that adds color white/transparent to the navbar on resize (this is for the collapse)
+	const updateColor = () => {
+		if (window.innerWidth < 993 && collapseOpen) {
+			setcolor('bg-white');
+		} else {
+			setcolor('navbar-transparent');
+		}
+	};
+	// this function opens and closes the collapse on small devices
+	const toggleCollapse = () => {
+		if (collapseOpen) {
+			setcolor('navbar-transparent');
+		} else {
+			setcolor('bg-white');
+		}
+		setcollapseOpen(!collapseOpen);
+	};
+	// this function is to open the Search modal
+	const toggleModalSearch = () => {
+		setmodalSearch(!modalSearch);
+	};
 
-  
-  return (
-    <>
-      <Navbar className={classNames("navbar-absolute", color)} expand="lg">
-        <Container fluid>
-          <div className="navbar-wrapper">
-            <div
-              className={classNames("navbar-toggle d-inline", {
-                toggled: props.sidebarOpened,
-              })}
-            >
-              <NavbarToggler onClick={props.toggleSidebar}>
-                <span className="navbar-toggler-bar bar1" />
-                <span className="navbar-toggler-bar bar2" />
-                <span className="navbar-toggler-bar bar3" />
-              </NavbarToggler>
-            </div>
-            <NavbarBrand href="#" onClick={(e) => e.preventDefault()}>
-              {props.brandText}
-            </NavbarBrand>
-          </div>
-          <NavbarToggler onClick={toggleCollapse}>
-            <span className="navbar-toggler-bar navbar-kebab" />
-            <span className="navbar-toggler-bar navbar-kebab" />
-            <span className="navbar-toggler-bar navbar-kebab" />
-          </NavbarToggler>
-          <Collapse navbar isOpen={collapseOpen}>
-            <Nav className="ml-auto" navbar>
-              <InputGroup className="search-bar">
-                <Button color="link" onClick={toggleModalSearch}>
-                  <i className="tim-icons icon-zoom-split" />
-                  <span className="d-lg-none d-md-block">Search</span>
-                </Button>
-              </InputGroup>
-              <UncontrolledDropdown nav>
-                <DropdownToggle
-                  caret
-                  color="default"
-                  data-toggle="dropdown"
-                  nav
-                >
-                  <div className="notification d-none d-lg-block d-xl-block" />
-                  <i className="tim-icons icon-sound-wave" />
-                  <p className="d-lg-none">Notifications</p>
-                </DropdownToggle>
-                <DropdownMenu className="dropdown-navbar" right tag="ul">
-                  <NavLink tag="li">
-                    <DropdownItem className="nav-item">
-                      Mike John responded to your email
-                    </DropdownItem>
-                  </NavLink>
-                  <NavLink tag="li">
-                    <DropdownItem className="nav-item">
-                      You have 5 more tasks
-                    </DropdownItem>
-                  </NavLink>
-                  <NavLink tag="li">
-                    <DropdownItem className="nav-item">
-                      Your friend Michael is in town
-                    </DropdownItem>
-                  </NavLink>
-                  <NavLink tag="li">
-                    <DropdownItem className="nav-item">
-                      Another notification
-                    </DropdownItem>
-                  </NavLink>
-                  <NavLink tag="li">
-                    <DropdownItem className="nav-item">
-                      Another one
-                    </DropdownItem>
-                  </NavLink>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-              {isAuthenticated ? <SignedIn/> : <NotLoggedIn/>}
+	return (
+		<>
+			<Navbar className={classNames('navbar-absolute', color)} expand="lg">
+				<Container fluid>
+					<div className="navbar-wrapper">
+						<div
+							className={classNames('navbar-toggle d-inline', {
+								toggled: props.sidebarOpened
+							})}>
+							<NavbarToggler onClick={props.toggleSidebar}>
+								<span className="navbar-toggler-bar bar1" />
+								<span className="navbar-toggler-bar bar2" />
+								<span className="navbar-toggler-bar bar3" />
+							</NavbarToggler>
+						</div>
+						<NavbarBrand href="#" onClick={e => e.preventDefault()}>
+							{props.brandText}
+						</NavbarBrand>
+					</div>
+					<NavbarToggler onClick={toggleCollapse}>
+						<span className="navbar-toggler-bar navbar-kebab" />
+						<span className="navbar-toggler-bar navbar-kebab" />
+						<span className="navbar-toggler-bar navbar-kebab" />
+					</NavbarToggler>
+					<Collapse navbar isOpen={collapseOpen}>
+						<Nav className="ml-auto" navbar>
+							<InputGroup className="search-bar">
+								<Button color="link" onClick={toggleModalSearch}>
+									<i className="tim-icons icon-zoom-split" />
+									<span className="d-lg-none d-md-block">Search</span>
+								</Button>
+							</InputGroup>
+							<UncontrolledDropdown nav>
+								<DropdownToggle caret color="default" data-toggle="dropdown" nav>
+									<div className="notification d-none d-lg-block d-xl-block" />
+									<i className="tim-icons icon-sound-wave" />
+									<p className="d-lg-none">Notifications</p>
+								</DropdownToggle>
+								<DropdownMenu className="dropdown-navbar" right tag="ul">
+									<NavLink tag="li">
+										<DropdownItem className="nav-item">
+											Mike John responded to your email
+										</DropdownItem>
+									</NavLink>
+									<NavLink tag="li">
+										<DropdownItem className="nav-item">You have 5 more tasks</DropdownItem>
+									</NavLink>
+									<NavLink tag="li">
+										<DropdownItem className="nav-item">Your friend Michael is in town</DropdownItem>
+									</NavLink>
+									<NavLink tag="li">
+										<DropdownItem className="nav-item">Another notification</DropdownItem>
+									</NavLink>
+									<NavLink tag="li">
+										<DropdownItem className="nav-item">Another one</DropdownItem>
+									</NavLink>
+								</DropdownMenu>
+							</UncontrolledDropdown>
+							{/* {isAuthenticated ? <SignedIn/> : <NotLoggedIn/>} */}
+							{isAuthenticated ? <SignedIn /> : ''}
 
-
-
-              <li className="separator d-lg-none" />
-            </Nav>
-          </Collapse>
-        </Container>
-      </Navbar>
-      <Modal
-        modalClassName="modal-search"
-        isOpen={modalSearch}
-        toggle={toggleModalSearch}
-      >
-        <ModalHeader>
-          <Input placeholder="SEARCH" type="text" />
-          <button
-            aria-label="Close"
-            className="close"
-            onClick={toggleModalSearch}
-          >
-            <i className="tim-icons icon-simple-remove" />
-          </button>
-        </ModalHeader>
-      </Modal>
-    </>
-  );
+							<li className="separator d-lg-none" />
+						</Nav>
+					</Collapse>
+				</Container>
+			</Navbar>
+			<Modal modalClassName="modal-search" isOpen={modalSearch} toggle={toggleModalSearch}>
+				<ModalHeader>
+					<Input placeholder="SEARCH" type="text" />
+					<button aria-label="Close" className="close" onClick={toggleModalSearch}>
+						<i className="tim-icons icon-simple-remove" />
+					</button>
+				</ModalHeader>
+			</Modal>
+		</>
+	);
 }
 
 export default HeadNavbar;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,70 +1,68 @@
-
-import Dashboard from "./views/Dashboard.js";
-import Icons from "./views/Icons.js";
-import Notifications from "./views/Notifications.js";
-import Typography from "./views/Typography.js";
-import UserProfile from "./views/UserProfile.js";
-import Offers from "./views/Offers.js"
-import Registration from "./views/Registration.js"
+import Dashboard from './views/Dashboard.js';
+import Icons from './views/Icons.js';
+import Notifications from './views/Notifications.js';
+import Typography from './views/Typography.js';
+import UserProfile from './views/UserProfile.js';
+import Offers from './views/Offers.js';
+import Registration from './views/Registration.js';
+import AuthModal from './views/LoginModal/AuthModal.js';
 
 var routes = [
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    icon: "tim-icons icon-chart-pie-36",
-    component: Dashboard,
-    layout: "/p",
-    
-  },
-  {
-    path: "/registration",
-    name: "Registration",
-    icon: "tim-icons icon-user-run",
-    component: Registration,
-    layout: "/p",
-    
-  },
-  {
-    path: "/offers",
-    name: "Offers",
-    icon: "tim-icons icon-credit-card",
-    component: Offers,
-    layout: "/p",
-   
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "tim-icons icon-atom",
-    component: Icons,
-    layout: "/p",
-   
-  },
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: "tim-icons icon-bell-55",
-    component: Notifications,
-    layout: "/p",
-    
-  },
-  {
-    path: "/user-profile",
-    name: "User Profile",
-    icon: "tim-icons icon-single-02",
-    component: UserProfile,
-    layout: "/p",
-    
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: "tim-icons icon-align-center",
-    component: Typography,
-    layout: "/p",
-   
-  },
-
- 
+	{
+		path: '/dashboard',
+		name: 'Dashboard',
+		icon: 'tim-icons icon-chart-pie-36',
+		component: Dashboard,
+		layout: '/p'
+	},
+	{
+		path: '/authentication',
+		name: 'Authentication',
+		icon: 'tim-icons icon-components',
+		component: AuthModal,
+		layout: '/p'
+	},
+	{
+		path: '/registration',
+		name: 'Registration',
+		icon: 'tim-icons icon-user-run',
+		component: Registration,
+		layout: '/p'
+	},
+	{
+		path: '/offers',
+		name: 'Offers',
+		icon: 'tim-icons icon-credit-card',
+		component: Offers,
+		layout: '/p'
+	},
+	{
+		path: '/icons',
+		name: 'Icons',
+		icon: 'tim-icons icon-atom',
+		component: Icons,
+		layout: '/p'
+	},
+	{
+		path: '/notifications',
+		name: 'Notifications',
+		icon: 'tim-icons icon-bell-55',
+		component: Notifications,
+		layout: '/p'
+	},
+	{
+		path: '/user-profile',
+		name: 'User Profile',
+		icon: 'tim-icons icon-single-02',
+		component: UserProfile,
+		layout: '/p'
+	},
+	{
+		path: '/typography',
+		name: 'Typography',
+		icon: 'tim-icons icon-align-center',
+		component: Typography,
+		layout: '/p'
+	}
 ];
 export default routes;
